416345b410a94e3d576fc487d5bf45e63795240b
LinkWu <wjianl@qq.com>
2017-01-25 00:26:26 +0800

    网络库

---
+++ network/postbuild.bat
@@ -0,0 +1,30 @@
+set TargetDir=%1
+set TargetName=%2
+set SolutionDir=%3
+
+set OUTPUT=%SolutionDir%\Plugins
+
+if not exist %OUTPUT% (
+    echo %OUTPUT% is not exist
+    goto END
+)
+
+set MONO_HOME=C:\Program Files\Unity\Editor\Data\MonoBleedingEdge
+set MONO_EXE=%MONO_HOME%\bin\mono.exe
+set PDB2MDB_EXE=%MONO_HOME%\lib\mono\4.5\pdb2mdb.exe
+
+if not exist "%MONO_EXE%" (
+    echo %MONO_EXE% is not exist
+    goto END
+)
+
+if not exist "%PDB2MDB_EXE%" (
+    echo %PDB2MDB_EXE% is not exist
+    goto END
+)
+
+"%MONO_EXE%" "%PDB2MDB_EXE%" %TargetDir%%TargetName%.dll
+copy %TargetDir%%TargetName%.dll %OUTPUT%\
+copy %TargetDir%%TargetName%.dll.mdb %OUTPUT%\
+
+:END
---
+++ network/src/NetworkManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace u3devtools.network
+{
+    class NetworkManager
+    {
+
+    }
+}
